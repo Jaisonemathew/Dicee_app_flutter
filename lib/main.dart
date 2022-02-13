1fc475c8810b96a97b4d78eac3fabfe1cc563a7c
Jaisonemathew <90517885+Jaisonemathew@users.noreply.github.com>
2022-02-13 15:22:05 +0530

    Flutter app made to roll dice on android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +18,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int dice1 = Random.secure().nextInt(6) + 1;
+  int dice2 = Random.secure().nextInt(6) + 1;
+
+  void rollTheDice() {
+    setState(() {
+      dice1 = Random.secure().nextInt(6) + 1;
+      dice2 = Random.secure().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$dice1.png'),
+              onPressed: () {
+                rollTheDice();
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$dice2.png'),
+              onPressed: () {
+                rollTheDice();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
